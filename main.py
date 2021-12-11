e07bb778ceab22dd46e8f2e3314f75f554ed21d0
Ayush Giri <63806638+Ayushrobben@users.noreply.github.com>
2021-12-11 18:01:16 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# People to be invited
+
+with open("Input/Names/invited_names.txt", mode="r") as names_file:
+    people_name = names_file.readlines()
+    people_name_list = []
+    for name in people_name:
+        people_name_list.append(name.strip("\n"))
+
+
+# Reading the sample letter
+
+with open("Output/ReadyToSend/example.txt", mode="r") as sample:
+    sample_letter = sample.readlines()
+
+
+# Creating letters for each person
+
+for name in people_name_list:
+    with open(f"Output/ReadyToSend/{name}.txt", mode="w") as invitation:
+        final_message = []
+        for message in sample_letter:
+            if sample_letter.index(message) == 0:
+                final_message.append(f"Dear {name},\n")
+
+            elif sample_letter.index(message) == len(sample_letter) - 1:
+                final_message.append("Ayush")
+
+            else:
+                final_message.append(message)
+
+        for final_words in final_message:
+            invitation.write(final_words)
+
+
+
+
+
